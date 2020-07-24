0f439256551b4aad1f9fb9d4d6a88ddd48ffc844
Joseph Odhiambo <josephkdo@gmail.com>
2020-07-24 07:33:41 +0300

    overlay mousehover text on images

--- js/myScript.js
+++ js/myScript.js
@@ -36,11 +36,44 @@ $(document).ready(function(){
   }).mouseout(function(){
     $("#overlay").hide();
   });
-});
-$(document).ready(function(){
   $("#work2").mouseover(function(){
     $("#overlay2").show();
   }).mouseout(function(){
     $("#overlay2").hide();
   });
+  $("#work3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+  $("#work5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+  $("#work6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+  $("#work7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+  $("#work8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
+});
+
+
+$(document).ready(function(){
+  
 });
