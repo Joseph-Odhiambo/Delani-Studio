cf015626772c598639cab53c7ee61c59808449fe
Joseph Odhiambo <josephkdo@gmail.com>
2020-07-24 08:11:30 +0300

    input validation

--- js/myScript.js
+++ js/myScript.js
@@ -73,7 +73,20 @@ $(document).ready(function(){
   });
 });
 
-
+//form input validation
 $(document).ready(function(){
-  
+  $("form#formValidity").submit(function(event){
+    // event.preventDefault();
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+    
+  });
+
 });
