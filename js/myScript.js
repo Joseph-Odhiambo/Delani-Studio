d8f0205051211817ea2efe6a4449a9d7ef1eaa17
Joseph Odhiambo <josephkdo@gmail.com>
2020-07-24 07:07:01 +0300

    design click show

---
+++ js/myScript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown().hide()
+      $("#design").show();
+    });
+    $("#design").click(function(){
+      $("#design").slideUp();
+      $("#design-image").slideDown();
+    });
+  });
