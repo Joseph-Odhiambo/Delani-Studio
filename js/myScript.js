c991459bab883b4ae3518de2096af7aece6994e3
Joseph Odhiambo <josephkdo@gmail.com>
2020-07-24 07:26:00 +0300

    first image text overlay

--- js/myScript.js
+++ js/myScript.js
@@ -29,3 +29,18 @@ $(document).ready(function(){
     $("#product-image").slideDown();
   });
 });
+//poertfolio text overlay images
+$(document).ready(function(){
+  $("#work1").mouseover(function(){
+    $("#overlay").show();
+  }).mouseout(function(){
+    $("#overlay").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+});
